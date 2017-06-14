39483ba348e7cdf2b1823a5eafec67c130d4c059
Zoltán Szarvas <zoltan.szarvas@neting.hu>
2017-06-14 22:28:30 +0200

    UTF8 charset and collation

--- start.sh
+++ start.sh
@@ -29,7 +29,7 @@ if [ ! -f /var/www/html/moodle/config.php ]; then
 
   mysqladmin -u root password $MYSQL_PASSWORD
   mysql -uroot -p$MYSQL_PASSWORD -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' WITH GRANT OPTION; FLUSH PRIVILEGES;"
-  mysql -uroot -p$MYSQL_PASSWORD -e "CREATE DATABASE moodle; GRANT ALL PRIVILEGES ON moodle.* TO 'moodle'@'localhost' IDENTIFIED BY '$MOODLE_PASSWORD'; FLUSH PRIVILEGES;"
+  mysql -uroot -p$MYSQL_PASSWORD -e "CREATE DATABASE moodle CHARACTER SET utf8 COLLATE utf8_general_ci; GRANT ALL PRIVILEGES ON moodle.* TO 'moodle'@'localhost' IDENTIFIED BY '$MOODLE_PASSWORD'; FLUSH PRIVILEGES;"
   killall mysqld
 fi
 # start all the services
